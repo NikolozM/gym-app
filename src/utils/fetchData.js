789fb32ef81e6e3a08ac75f3fd1b63b8d614ac36
Nikoloz <mazmishvilinika888@gmail.com>
2022-08-03 01:13:46 +0400

    home page components, fetch data

---
+++ src/utils/fetchData.js
@@ -0,0 +1,15 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key":
+      "bffa45d8c8mshc12d270e3f30c7cp189ad2jsn0e8faf171124",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+};
